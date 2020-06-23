796f613118f225ef65b64c5dce85e148167ff0b9
Dayana Mardari <dayana_mdr@yahoo.com>
2020-06-23 13:43:53 +0300

    discover, login, logout, register paths update

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from users import views as user_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('discover.urls')),
     path('', include('users.urls')),
-    path('discover/', include('discover.urls')),
+    path('register/', user_views.registerPage ,name = 'register'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html', redirect_authenticated_user = True), name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout'),
 ]
 
 if settings.DEBUG:
--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 from . import views
 from users import views as user_views
-from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('@<str:username>/', user_views.view_user_profile, name = 'user-profile'),
-    path('register/', user_views.registerPage ,name = 'register'),
-    path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html', redirect_authenticated_user = True), name = 'login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout'),
-
 ]
