a778ad6526267cca4da509ea1578c33149962815
Dayana Mardari <dayana_mdr@yahoo.com>
2020-07-01 21:47:10 +0300

    create and view posts feature

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, UserProfile, UserPost
+from .models import User, UserProfile
 
 admin.site.register(User, UserAdmin)
 admin.site.register(UserProfile)
-admin.site.register(UserPost)
